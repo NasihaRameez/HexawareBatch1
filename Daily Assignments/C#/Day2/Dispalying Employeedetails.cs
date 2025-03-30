fd5b8a782030c33309cab3484c266caee71c6172
NasihaRameez <nasiharameez03@gmail.com>
2025-03-30 16:28:13 +0530

    Track of Training

---
+++ Daily Assignments/C#/Day2/Dispalying Employeedetails.cs
@@ -0,0 +1,42 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+
+            Console.Write("Enter Employee ID: ");
+            emp.Id = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Employee Name: ");
+            emp.Name = Console.ReadLine();
+
+            Console.Write("Enter Designation: ");
+            emp.Designation = Console.ReadLine();
+
+            Console.Write("Enter Salary: ");
+            emp.Salary = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\n====================\n");
+            emp.DisplayDetails();
+        }
+    }
+
+    class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Designation { get; set; }
+        public double Salary { get; set; }
+
+        public void DisplayDetails()
+        {
+            Console.WriteLine("Employee Details:");
+            Console.WriteLine("ID: " + Id);
+            Console.WriteLine("Name: " + Name);
+            Console.WriteLine("Designation: " + Designation);
+            Console.WriteLine("Salary: " + Salary);
+        }
+    }
+}
