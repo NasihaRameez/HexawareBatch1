7c4cd8f5e45bea2b6f872c6b616f59659cc85f4d
NasihaRameez <nasiharameez03@gmail.com>
2025-04-04 17:11:55 +0530

    Track of Training

---
+++ Daily Assignments/C#/Day6/Day6Assgn/Day6Assgn/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Day6Assgn
+{
+    public static class StringExtensions
+    {
+        public static int WordCount(this string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+                return 0;
+
+            string[] words = input.Split(new char[] { ' ', '\t', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            return words.Length;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = "how are you?";
+
+            int wordCount = sentence.WordCount();
+
+            Console.WriteLine($"The number of words in the sentence is: {wordCount}");
+
+            Console.ReadLine();
+        }
+    }
+}
+
